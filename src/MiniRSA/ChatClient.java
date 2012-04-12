ad470679791119422dbe36f81e30a43ee33a8b87
Michael Pato <mtpato@gmail.com>
2012-04-12 15:43:45 -0400

    added package and created class for chat client

---
+++ src/MiniRSA/ChatClient.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package MiniRSA;
+
+/**
+ * @author michaelpato
+ *
+ */
+public class ChatClient {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
