bbac63bd52dc77ad6236cdff611f56599af83c45
Michael Pato <mtpato@gmail.com>
2012-04-16 11:35:53 -0400

    added all the test stubs

--- src/MiniRSA/RSATest.java
+++ src/MiniRSA/RSATest.java
@@ -1,12 +1,80 @@
+/**
+ * 
+ */
 package MiniRSA;
 
+import static org.junit.Assert.*;
 
 import org.junit.Before;
+import org.junit.Test;
 
+/**
+ * @author michaelpato
+ *
+ */
 public class RSATest {
 
+    /**
+     * @throws java.lang.Exception
+     */
     @Before
     public void setUp() throws Exception {
+    }
+
+    /**
+     * Test method for {@link MiniRSA.RSA#RSA()}.
+     */
+    @Test
+    public void testRSA() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link MiniRSA.RSA#coprime(int)}.
+     */
+    @Test
+    public void testCoprime() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link MiniRSA.RSA#endecrypt(long, long, int)}.
+     */
+    @Test
+    public void testEndecrypt() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link MiniRSA.RSA#GCD(long, long)}.
+     */
+    @Test
+    public void testGCD() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link MiniRSA.RSA#mod_inverse(long, long)}.
+     */
+    @Test
+    public void testMod_inverse() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link MiniRSA.RSA#modulo(long, long, long)}.
+     */
+    @Test
+    public void testModulo() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link MiniRSA.RSA#totient(long)}.
+     */
+    @Test
+    public void testTotient() {
+        fail("Not yet implemented");
     }
 
 }
